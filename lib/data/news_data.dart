d79fdde4b4ac893c0c10c1c410d6a1fec2d9aac0
unknown <imonbdcallingit@gmail.com>
2024-03-01 18:33:55 +0600

    all news are come form api

--- lib/data/news_data.dart
+++ lib/data/news_data.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
+import 'package:am_innnn/utils/api_url.dart';
 import 'package:http/http.dart' as http;
 import '../model/news_model.dart';
 
 class NewsData {
   static Future<NewsModel> fetchAllNews() async {
     try{
-      final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines?language=en&country=us&apiKey=30099d4c210a4289b8bc9a9e6430ecb1'));
+      final response = await http.get(Uri.parse(ApiUrl.allNewsUrl));
 
       if (response.statusCode == 200) {
         // If the server returns a 200 OK response, parse the JSON
